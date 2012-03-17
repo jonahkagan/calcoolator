5977ff92bd6b55cb872aebf3d590318901eccfed
Jessica Liu <jessica.sichen.liu@gmail.com>
2012-03-17 11:27:52 -0400

    can manipulate linear function, supports multiple graphs, graphs selectable

--- draw-graph.js
+++ draw-graph.js
@@ -1,29 +1,76 @@
 TEST = {};
 function drawGraph(p) {
     var UNIT = 20,
-        STEP = 2,
+        STEP = 1,
         START_W = 600,
         START_H = 600,
+        ANCHOR_SIZE = 10,
+        EPSILON = 10,
         originX, originY,
-        currentFunction;    
+        functions = [],
+        selectedFunction,
+        funAnchors,
+        currAnchor,
+        i0, i1, i2;    
 
     p.setup = function () {
         p.size(START_W, START_H);
         originX = p.width/2;
         originY = p.height/2;
-        currentFunction = makeFun("f", [0,1,1,4,2]);
-        drawGrid();
-        currentFunction.draw(); 
+        i0 = document.getElementById("zero");
+        i1 = document.getElementById("one");
+        i2 = document.getElementById("two");
+        i0.onchange = readTemporaryInput;
+        i1.onchange = readTemporaryInput;
+        i2.onchange = readTemporaryInput;
+        readTemporaryInput();
+        p.smooth();
+        drawStuff();
     };
 
-    p.draw = function () {
-    };
+    p.draw = function () { };
+
+    drawStuff = function() {
+        drawGrid();
+        for (f in functions) {
+            functions[f].draw();
+        }
+    }
+    
+    p.mouseClicked = function() {
+        selectFunction();
+    }
+
+    p.mousePressed = function() {
+        selectedFunction.mousePressed();
+    }
+
+    p.mouseDragged = function() {
+        selectedFunction.mouseDragged();
+    }
+
+    p.mouseReleased = function() {
+        selectedFunction.mouseReleased();
+    }
+    
+    function readTemporaryInput() {
+        selectedFunction = makeFun("f", [i0.value, i1.value, i2.value]);
+        functions.push(selectedFunction);
+        drawStuff();
+    }
+    
+    function writeTemporaryInput() {
+        i0.value = selectedFunction.coefs[0];
+        i1.value = selectedFunction.coefs[1];
+        i2.value = selectedFunction.coefs[2];
+    }
 
     function drawGrid() {
         p.background(250);
-
+        
         // Gridlines
         p.stroke(230);
+        p.strokeWeight(1);
         for (x = 0; x <= originX; x += UNIT) {
             p.line(originX + x, 0, originX + x, p.height);
             p.line(originX - x, 0, originX - x, p.height);
@@ -38,6 +85,20 @@ function drawGraph(p) {
         p.line(originX, 0, originX, p.height);
         p.line(0, originY, p.width, originY);
     }
+    
+    function getNextFunctionColor() {
+        // replace with something smart, k?
+        return p.color(p.random(255), p.random(255), p.random(255));
+    }
+    
+    function selectFunction() {
+        for (f in functions) {
+            if (functions[f].isClicked()) {
+                selectedFunction = functions[f];
+            }
+        }
+        drawStuff();
+    }
 
     // A function has
     // - coefficient list
@@ -49,22 +110,15 @@ function drawGraph(p) {
         var fun = {
             name: name,
             coefs: coefs,
-            color: p.color(255,0,0)
+            anchors: {},
+            color: getNextFunctionColor()
         };
 
-        fun.draw = function() {
-            p.stroke(fun.color);
-            var p1 = makePoint({px:0, py:0});
-            var p2;
-            p1.uy(fun.evaluate(p1.ux()));
-            
-            while(p1.px() < p.width) {
-                p2 = makePoint({px:p1.px() + STEP, py:0});
-                p2.uy(fun.evaluate(p2.ux()));
-                p.line(p1.px(), p1.py(), p2.px(), p2.py());
-                p1 = p2;
-            }
-        };
+        fun.degree = 0;
+        for (var i = 0; i < coefs.length; i++) {
+            if (coefs[i] != 0)
+                fun.degree = i;
+        }
 
         fun.evaluate = function(x) {
             var fx = 0;
@@ -77,6 +131,63 @@ function drawGraph(p) {
             return fx;
         }
 
+        fun.draw = function() {
+            p.stroke(fun.color);
+            p.strokeWeight(fun == selectedFunction ? 2 : 1);
+            var p1 = makePoint({px:0, py:0});
+            var p2;
+            p1.uy(fun.evaluate(p1.ux()));
+            
+            while(p1.px() < p.width) {
+                p2 = makePoint({px:p1.px() + STEP, py:0});
+                p2.uy(fun.evaluate(p2.ux()));
+                p.line(p1.px(), p1.py(), p2.px(), p2.py());
+                p1 = p2;
+            }
+
+            if (fun == selectedFunction) {
+                for (var a in fun.anchors) {
+                    fun.anchors[a].draw();
+                }
+            }
+        };
+        
+        fun.isClicked = function() {
+            var pt = makePoint({px:0, py:0});
+            pt.uy(fun.evaluate(pt.ux()));
+            while(pt.px() < p.width) {
+                if (p.dist(pt.px(), pt.py(), p.mouseX, p.mouseY) < EPSILON) {
+                    return true;
+                }
+                pt.px(pt.px() + STEP);
+                pt.uy(fun.evaluate(pt.ux()));
+            }
+            return false;
+        }
+
+        if (fun.degree === 1) {
+            fun.anchors.translate = makeTranslateAnchor(fun, {ux:0, uy:fun.evaluate(0)});
+            fun.anchors.rotate = makeRotateAnchor(fun, {px: originX + p.width * .1, py: 0});
+            fun.anchors.rotate.uy(fun.evaluate(fun.anchors.rotate.ux()));
+        }
+
+        fun.mousePressed = function() {
+            currAnchor = null;
+            for (var a in fun.anchors) {
+                if (fun.anchors[a].isPressed()) {
+                    currAnchor = fun.anchors[a];
+                }
+            }
+        }
+
+        fun.mouseDragged = function() {
+            currAnchor && currAnchor.onDrag();
+            drawStuff(); 
+        }
+
+        fun.mouseReleased = function() {
+
+        }
         return fun;
     }
 
@@ -156,6 +267,55 @@ function drawGraph(p) {
         }
 
         return pt;
+    }
+    
+    function makeAnchor(fun, spec) {
+        var anchor = makePoint(spec);
+        anchor.fun = fun;
+        
+        anchor.draw = function() {
+            p.stroke(anchor.fun.color);
+            p.ellipseMode(p.CENTER);
+            p.ellipse(anchor.px(), anchor.py(), ANCHOR_SIZE, ANCHOR_SIZE);
+            p.fill(anchor.fun.color);
+        }
+        
+        anchor.isPressed = function() {
+            return p.dist(anchor.px(), anchor.py(), p.mouseX, p.mouseY) < EPSILON;
+        };
+        
+        anchor.onDrag = function() {};
+        
+        return anchor
+    }
+    
+    function makeTranslateAnchor(fun, spec) {
+        var anchor = makeAnchor(fun, spec);
+        anchor.onDrag = function() {
+            var dy = p.mouseY - anchor.py();
+            for (a in anchor.fun.anchors) {
+                anchor.fun.anchors[a].py(anchor.fun.anchors[a].py() + dy);
+            }
+            anchor.fun.coefs[0] = anchor.uy();
+            writeTemporaryInput();
+        }
+        return anchor;
+    }
+    
+    function makeRotateAnchor(fun, spec) {
+        if (fun.degree != 1) {
+            die("cannot make rotate anchor for degree not equal to one", fun)
+        }
+        var rotate = makeAnchor(fun, spec);
+        rotate.onDrag = function() {
+            rotate.py(p.mouseY);
+            rotate.px(p.mouseX);
+            translate = fun.anchors.translate;
+            var slope = (rotate.uy() - translate.uy())/(rotate.ux() - translate.ux());
+            rotate.fun.coefs[1] = slope;
+            writeTemporaryInput();
+        }
+        return rotate;
     }
 
     function die(msg, obj) { console.log(msg); }
