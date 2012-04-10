b8169768e7bedec399e4f986ddc14859a4e8b853
jonahkagan <jonahkagan@gmail.com>
2012-04-09 21:39:18 -0400

    more reorg

--- js/views/eqn-view.js
+++ js/views/eqn-view.js
@@ -1,4 +1,4 @@
-function makeEqnEditor() {
+function makeEqnView() {
     var me = G.makeRepView();
 
     // Displays a text field with a function name f(x)
@@ -8,60 +8,24 @@ function makeEqnEditor() {
     //
     // Number coefficients in the equation are scrubbable
 
+    me.display = function (fun, $parent) {
+
+         
+    }
+
     var $ed, $text, $display, eqnStr;
-
-    (function init() {
-        $ed = $(
-            '<div class="eqneditor" tabindex="1">' +
-                // Invisible text area catches keypresses
-                '<textarea class="eqntext"></textarea>' +
-                // which we display behind it
-                '<div class="eqndisplay"></div>' +
-            '</div>'
-        )
-        .keyup(handleKey);
-
-        $text = $ed.find('.eqntext');
-        $display = $ed.find('.eqndisplay');
-
-        $ed.on('click', 'span', function (e) { console.log(e); });
-    }());
-
-    // Returns the JQuery object to be inserted into the DOM
-    me.getContent = function () {
-        return $ed;
-    };
-
     function handleKey(e) {
-        console.log('key');
+        console.log("key");
         eqnStr = $text.val();
         $display.html(eqnToHTML(eqnStr));
         var coefs = parser.parseAndSimplify(eqnStr);
-        $('#out').html(coefs ? coefs.toString() : 'parse error');
+        $("#out").html(coefs ? coefs.toString() : "parse error");
     }
 
-    // Takes an eqn str and returns an HTML string
-    // where each token in the eqn is represented by
-    // an element.
-    function eqnToHTML(eqnStr) {
-        return _.map(parser.tokens(eqnStr),
-            function (tok) {
-                switch (tok.type) {
-                    case parser.T.NUMBER:
-                        return '<span class="num">' + tok.value +
-                            '</span>';
-                    case parser.T.OPERATOR:
-                        return '<span class="op">' + tok.value +
-                            '</span>';
-                    case parser.T.ID:
-                        return '<span class="id">' + tok.value +
-                            '</span>';
-                    case parser.T.BAD:
-                        return '<span class="bad">' + tok.value +
-                            '</span>';
-                }
-            }).join('');
-    };
+    function toEqnString(coefs) {
+        _.reduceRight(coefs, function (coef) {
+        }, "");
+    }
 
     return me;
 }
